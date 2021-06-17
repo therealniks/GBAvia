e0e8fef449bf214c13e9d1b25fb76286a24e85b4
therealniks <therealniks@gmail.com>
2021-06-17 23:41:10 +0300

    creating project

---
+++ GBAvia/GBAvia/MainViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainViewController.h
+//  GBAvia
+//
+//  Created by Nikita on 17.06.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+
+@end
+
---
+++ GBAvia/GBAvia/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainViewController.m
+//  GBAvia
+//
+//  Created by Nikita on 17.06.2021.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.cyanColor;
+}
+
+
+@end
