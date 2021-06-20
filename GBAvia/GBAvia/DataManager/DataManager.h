d939eb75036c85aaaa468b6ff291c51dd6179861
therealniks <therealniks@gmail.com>
2021-06-20 16:50:55 +0300

    adding DataManager
    loading data from JSON

--- GBAvia/GBAvia/DataManager/DataManager.h
+++ GBAvia/GBAvia/DataManager/DataManager.h
@@ -6,10 +6,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Country.h"
+#import "City.h"
+#import "Airport.h"
+
+#define kDataManagerLoadDataDidComplete @"DataManagerLoadDataDidComplete"
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef enum DataSourceType{
+    DataSourceTypeCountry,
+    DataSourceTypeCity,
+    DataSourceTypeAirport
+} DataSourceType;
+
+
 @interface DataManager : NSObject
+
+@property(nonatomic, strong, readonly) NSArray *country;
+@property(nonatomic, strong, readonly) NSArray *city;
+@property(nonatomic, strong, readonly) NSArray *airport;
+
++(instancetype) sharedInstance;
+
+-(void) loadData;
 
 @end
 
