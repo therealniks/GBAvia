6f4d24705b796ad565f9533313b0a9206a5a7efe
therealniks <therealniks@gmail.com>
2021-06-30 19:13:16 +0300

    adding LocationService

--- GBAvia/GBAvia/Service/APIManager.m
+++ GBAvia/GBAvia/Service/APIManager.m
@@ -11,6 +11,7 @@
 #define API_URL_IP_ADDRESS @"https://api.ipify.org/?format=json"
 #define API_URL_CHEAP @"https://api.travelpayouts.com/v1/prices/cheap"
 #define API_URL_CITY_FROM_IP @"https://www.travelpayouts.com/whereami?ip="
+#define API_URL_MAP_PRICE @"https://map.aviasales.ru/prices.json?origin_iata="
 
 
 @implementation APIManager
@@ -92,6 +93,27 @@ NSString * SearchRequestQuery(SearchRequest request) {
         [dateFormatter stringFromDate:request.departDate], [dateFormatter stringFromDate:request.returnDate]];
     }
     return result;
+}
+
+- (void)mapPricesFor:(City *)origin withCompletion:(void (^)(NSArray *prices))completion
+{
+    static BOOL isLoading;
+    if (isLoading) { return; }
+    isLoading = YES;
+    [self load:[NSString stringWithFormat:@"%@%@", API_URL_MAP_PRICE, origin.code] withCompletion:^(id  _Nullable result) {
+        NSArray *array = result;
+        NSMutableArray *prices = [NSMutableArray new];
+        if (array) {
+            for (NSDictionary *mapPriceDictionary in array) {
+                MapPrice *mapPrice = [[MapPrice alloc] initWithDictionary:mapPriceDictionary withOrigin:origin];
+                [prices addObject:mapPrice];
+            }
+            isLoading = NO;
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completion(prices);
+            });
+        }
+    }];
 }
 
 @end
---
+++ GBAvia/GBAvia/Service/LocationService.h
@@ -0,0 +1,19 @@
+//
+//  LocationService.h
+//  GBAvia
+//
+//  Created by Nikita on 30.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+#define kLocationServiceDidUpdateCurrentLocation @"LocationServiceDidUpdateCurrentLocation"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocationService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
