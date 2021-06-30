5e5ca0430044a66344f3c05a67845d0e4812b249
therealniks <therealniks@gmail.com>
2021-06-30 21:22:58 +0300

    add mapView button

--- GBAvia/GBAvia/DataManager/DataManager.m
+++ GBAvia/GBAvia/DataManager/DataManager.m
@@ -97,7 +97,7 @@
 - (City *)cityForLocation:(CLLocation *)location {
     for (City *city in _citiesArray) {
         if (ceilf(city.coordinate.latitude) == ceilf(location.coordinate.latitude) && ceilf(city.coordinate.longitude) == ceilf(location.coordinate.longitude)) {
-            return city;
+        return city;
         }
     }
     return nil;
--- GBAvia/GBAvia/View/MapViewController/MapViewController.m
+++ GBAvia/GBAvia/View/MapViewController/MapViewController.m
@@ -11,58 +11,53 @@
 #import <MapKit/MapKit.h>
 #import "DataManager.h"
 #import "MapPrice.h"
-#import <CoreLocation/CoreLocation.h
+#import <CoreLocation/CoreLocation.h>
 
-@interface MapViewController ()
-@property(nonatomic, strong) MKMapView *mapView;
-@property(nonatomic, strong) LocationService *locationService;
-@property(nonatomic, strong) City *origin;
-@property(nonatomic, strong) NSArray *prices;
+@interface MapViewController () <MKMapViewDelegate>
+@property (strong, nonatomic) MKMapView *mapView;
+@property (nonatomic, strong) LocationService *locationService;
+@property (nonatomic, strong) City *origin;
+@property (nonatomic, strong) NSArray *prices;
 @end
 
 @implementation MapViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"Prices Map";
-    
+    self.title = @"Карта цен";
     _mapView = [[MKMapView alloc] initWithFrame:self.view.bounds];
     _mapView.showsUserLocation = YES;
     [self.view addSubview:_mapView];
-    
     [[DataManager sharedInstance] loadData];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dataLoadedSuccessfully) name:kDataManagerLoadDataDidComplete object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateCurrentLocation:) name:kLocationServiceDidUpdateCurrentLocation object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dataLoadedSuccessfully)
+    name:kDataManagerLoadDataDidComplete object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateCurrentLocation:)
+    name:kLocationServiceDidUpdateCurrentLocation object:nil];
 }
 
-- (void) dealloc {
+- (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (void) dataLoadedSuccessfully {
-    _locationService = [LocationService new];
+- (void)dataLoadedSuccessfully {
+    _locationService = [[LocationService alloc] init];
 }
-
-- (void) updateCurrentLocation: (NSNotification *)notification {
+- (void)updateCurrentLocation:(NSNotification *)notification {
     CLLocation *currentLocation = notification.object;
-    
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(currentLocation.coordinate, 1000000, 1000000);
-    [_mapView setRegion:region animated:YES];
+    [_mapView setRegion: region animated: YES];
     if (currentLocation) {
-        _origin = [[DataManager sharedInstance] cityForIATA:currentLocation];
+        _origin = [[DataManager sharedInstance] cityForLocation:currentLocation];
         if (_origin) {
-            [[APIManager sharedInstance] mapPricesFor:_origin withCompletion:^(NSArray * _Nonnull prices) {
-                            self.prices = prices;
+        [[APIManager sharedInstance] mapPricesFor:_origin withCompletion:^(NSArray *prices) {
+            self.prices = prices;
             }];
         }
     }
 }
-
 - (void)setPrices:(NSArray *)prices {
     _prices = prices;
     [_mapView removeAnnotations: _mapView.annotations];
- 
     for (MapPrice *price in prices) {
         dispatch_async(dispatch_get_main_queue(), ^{
             MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
@@ -73,17 +68,5 @@
         });
     }
 }
-
-
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
+    
 @end
